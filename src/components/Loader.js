15d8d89f54626c6214e2c3cd3e89fe87514a4c1b
Yoon sang seok <63184031+amamov@users.noreply.github.com>
2020-08-26 22:02:50 +0900

    c

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,8 +4,8 @@ import "../static/css/styles.css";
 
 const Loader = () => {
   return (
-    <div className="body w-375px h-screen items-center justify-center">
-      <div className="text-sm absolute z-50">
+    <div className="body w-375px h-667px items-center justify-center bg-black">
+      <div className="absolute z-50">
         {/* <CubeGrid color="#f26431" size="30px" /> */}
         <CubeGrid color="#ffffff" size="30px" />
         {/* <Circular color="#ffffff" size="30px" /> */}
