5569c20e5be4d5cb155b61f47f4424fa2b7c90b6
Yoon sang seok <63184031+amamov@users.noreply.github.com>
2020-08-27 21:22:50 +0900

    c

--- src/routes/Final.js
+++ src/routes/Final.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../static/css/styles.css";
+import Loader from "../components/Loader";
 
 const Final = () => {
+  const src = require("../images/final/bg.png");
+  const [isReady, setIsReady] = useState(false);
+  const onImageLoaded = () => {
+    setIsReady(true);
+  };
   return (
     <div className="body">
-      <div id="container" class="container justify-center">
+      {/* <div id="container" class="container justify-center">
         <Link to={{ pathname: "/intro" }} class="move-bt">
           다시 검사하기
         </Link>
@@ -17,7 +23,22 @@ const Final = () => {
         >
           텀블벅 바로가기
         </a>
-      </div>
+      </div> */}
+      <img
+        src={src}
+        alt="Lio"
+        title="Lio"
+        className="absolute w-375px z-0"
+        // onLoad={onImageLoaded}
+      />
+      <img
+        src={require("../images/final/ele.svg")}
+        alt="Lio"
+        title="Lio"
+        className="absolute w-375px z-10"
+        onLoad={onImageLoaded}
+      />
+      <div className="w-375px h-667px p-12 z-30">{!isReady && <Loader />}</div>
     </div>
   );
 };
