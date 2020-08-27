b984c9078884952a404e80f2d438368decf0a3e7
Yoon sang seok <63184031+amamov@users.noreply.github.com>
2020-08-28 00:46:24 +0900

    c

--- src/routes/ResultsPages/Result_D.js
+++ src/routes/ResultsPages/Result_D.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import Cube from "styled-loaders-react/lib/components/Cube";
 
 const Result_D = () => {
-  const src = require("../../images/Result/D.svg");
+  const src = require("../../images/Result/A.svg");
   const [isReady, setIsReady] = useState(false);
   const onImageLoaded = () => {
     setIsReady(true);
