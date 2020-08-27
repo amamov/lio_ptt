6cf52fb862ecc52ff1e2ea2128edc01b0037f18a
Yoon sang seok <63184031+amamov@users.noreply.github.com>
2020-08-27 23:26:42 +0900

    c

--- src/components/Prestart.js
+++ src/components/Prestart.js
@@ -3,28 +3,37 @@ import "../static/css/styles.css";
 import Loader from "./Loader";
 
 const Prestart = () => {
-  const src = require("../images/Start/preele.svg");
-  const [isReady, setIsReady] = useState(false);
-  const onImageLoaded = () => {
-    setIsReady(true);
+  const src1 = require("../images/Start/prebg.png");
+  const [isReady1, setIsReady1] = useState(false);
+  const onImageLoaded1 = () => {
+    setIsReady1(true);
+  };
+  const src2 = require("../images/Start/preele.svg");
+  const [isReady2, setIsReady2] = useState(false);
+  const onImageLoaded2 = () => {
+    setIsReady2(true);
   };
   return (
     <div className="flex flex-col relative w-375px h-1100px">
       <img
-        src={require("../images/Start/prebg.png")}
+        src={src1}
         alt="Lio"
         title="Lio"
         className="absolute w-375px z-0"
-        // onLoad={onImageLoaded}
+        onLoad={onImageLoaded1}
       />
       <img
-        src={src}
+        src={src2}
         alt="Lio"
         title="Lio"
         className="absolute w-375px z-10"
-        onLoad={onImageLoaded}
+        onLoad={onImageLoaded2}
       />
-      {!isReady && <Loader />}
+      {() => {
+        if (!isReady1 && !isReady2) {
+          return <Loader />;
+        }
+      }}
     </div>
   );
 };
