802e5832b695c980f8ecb6ef37e4e304df783f32
Yoon sang seok <63184031+amamov@users.noreply.github.com>
2020-08-27 18:27:59 +0900

    c

--- src/components/Prestart.js
+++ src/components/Prestart.js
@@ -5,18 +5,27 @@ import Loader from "./Loader";
 const Prestart = () => {
   const src = require("../images/Start/prebg.svg");
   const [isReady, setIsReady] = useState(false);
-  const onImageLoaded = () => {
-    setIsReady(true);
+  const cacheImages = (srcArray) => {
+    srcArray.forEach((src) => {
+      new Promise(function (resolve, reject) {
+        const image = new Image();
+        image.src = src;
+        image.onload = resolve();
+        image.onerror = reject();
+      });
+    });
+    setTimeout(() => setIsReady(true), 3000);
   };
+  useEffect(() => {
+    const images = [
+      require("../images/Start/bg2.svg"),
+      require("../images/Start/m.svg"),
+    ];
+    cacheImages(images);
+  }, []);
   return (
     <div className="flex flex-col relative w-375px h-1100px">
-      <img
-        src={src}
-        alt="Lio"
-        title="Lio"
-        className="absolute w-375px"
-        onLoad={onImageLoaded}
-      />
+      <img src={src} alt="Lio" title="Lio" className="absolute w-375px" />
       {!isReady && <Loader />}
     </div>
   );
